06031fc91813d5c77f91dada565dab5665386fda
Megan Black <blackme@oregonstate.edu>
2024-01-27 13:32:26 -0800

    added skeleton code for game

---
+++ tic_tac_toe.py
@@ -0,0 +1,68 @@
+"""
+This file contains skeleton code for implementing a tic tac toe game in python
+
+End result will be a 2-player interactive game
+
+"""
+
+def play_turn(player, board):
+    """
+    Execute a single turn
+    Inputs:
+        player - int (1 or 2)
+        board - 2D list
+    Outputs: None
+    """
+
+
+def check_win(board):
+    """
+    Check for any win
+    Inputs: 
+        board - 2D list 
+    Outputs: 
+        win - boolean
+    """
+    l = len(board)
+
+    # check rows    
+    
+    # check cols
+
+    # check main diagonal
+        
+    # check other diagonal
+
+
+def print_board(board):
+    """
+    Print current form of board
+    Inputs: 
+        board - 2D list 
+    Outputs: None
+    """
+
+    # Print column numbers
+
+    # Print board with row numbers
+
+
+
+def play_game():
+    """
+    Driver for gameplay
+    Inputs: None
+    Outputs: None
+    """
+    print("===============================================================")
+    print(" Welcome! We're going to be playing a fun game of tic-tac-toe!\n Make sure that you have 2 players ready :)")
+    print("===============================================================")
+
+    # Get valid inputs from user for board dimensions
+        
+    # Play game until win or tie condition is met
+
+
+
+if __name__ == "__main__":
+    play_game()
