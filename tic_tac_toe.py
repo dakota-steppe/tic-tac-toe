337eef07feac17708f5f9156843f621fb4363e0e
Dakota <dakotasteppe@gmail.com>
2024-01-27 14:23:36 -0800

    created function for taking user inputs for board size.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,7 +5,7 @@ End result will be a 2-player interactive game
 
 """
 
-def play_turn(player, board):
+def play_turn(player, board): # 3rd
     """
     Execute a single turn
     Inputs:
@@ -15,7 +15,7 @@ def play_turn(player, board):
     """
 
 
-def check_win(board):
+def check_win(board): # 4th
     """
     Check for any win
     Inputs:
@@ -23,7 +23,6 @@ def check_win(board):
     Outputs:
         win - boolean
     """
-
 
     # check rows
 
@@ -34,7 +33,7 @@ def check_win(board):
     # check other diagonal
 
 
-def print_board(board):
+def print_board(board): #2nd
     """
     Print current form of board
     Inputs:
@@ -48,7 +47,7 @@ def print_board(board):
 
 
 
-def play_game():
+def play_game(): #1st
     """
     Driver for gameplay
     Inputs: None
@@ -58,7 +57,19 @@ def play_game():
     print(" Welcome! We're going to be playing a fun game of tic-tac-toe!\n Make sure that you have 2 players ready :)")
     print("===============================================================")
 
-    # Get valid inputs from user for board dimensions
+    while True:
+        # Get valid inputs from user for board dimensions
+        # define variable, use input() function to get user input
+        user_dimension_input = input("Please enter your desired board size: ")
+        # check it's a number and greater than or equal to 3
+        if user_dimension_input.isdigit() and int(user_dimension_input) >= 3:
+            user_dimension_input = int(user_dimension_input)
+            break
+        else:
+            print("You gave invalid input, please try again with a number that is 3 or greater!")
+
+    # Define the board
+
 
     # Play game until win or tie condition is met
 
